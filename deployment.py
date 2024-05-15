d2a502a8cc22bafd7a271ce6cadad0a2c24b3808
Renzo Mendoza <143251538+MendozaJRL@users.noreply.github.com>
2024-05-15 14:59:42 +0800

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -21,11 +21,11 @@ length = 100
 size = (width, length)
 
 def import_and_predict(image_data,model):
-    image = ImageOps.fit(image_data, size, Image.Resampling.LANCZOS)
-    img = np.asarray(image)
-
-    img_reshape = img[np.newaxis, ...]
-    prediction = model.predict(img_reshape)
+    image=ImageOps.fit(image_data, size)
+    img=np.asarray(image)
+  
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
     return prediction
 
 if file is None:
