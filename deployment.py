284f4a6c64935192c72a002142134a323209ece1
Renzo Mendoza <143251538+MendozaJRL@users.noreply.github.com>
2024-05-15 15:42:20 +0800

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -3,45 +3,36 @@
 import streamlit as st
 import tensorflow as tf
 
+@st.cache_resource
+def load_model():
+  model = tf.keras.models.load_model('Model79.4.h5')
+  return model
+
+model = load_model()
+st.write("""# Agricultural Crops Classifier""")
+file = st.file_uploader("Choose crop photo from computer",type = ["jpg","png"])
+
 import cv2
 from PIL import Image,ImageOps
 import numpy as np
 
-from numpy import argmax
-from keras.preprocessing.image import load_img, img_to_array
-from keras.models import load_model
-
-@st.cache_resource
-def getmymodel():
-  model = tf.keras.models.load_model('Model83.h5')
-  return model
-
-model = getmymodel()
-st.write("""# Agricultural Crops Classifier""")
-file = st.file_uploader("Choose crop photo from computer",type = ["jpg","png"])
-
-def import_and_predict(image_data, model):
-    size=(100,100)
-    #image=ImageOps.fit(image_data, size)
-    image = load_img(image_data, grayscale=True, target_size=size)
-    img = img_to_array(image)
+def import_and_predict(image_data,model):
+    size=(64,64)
+    image=ImageOps.fit(image_data, size)
+    img=np.asarray(image)
+  
     img_reshape=img[np.newaxis,...]
-  
-    img = img.astype('float32')
-    img = img / 255.0
-  
-    predict_value = model.predict(img)
-    digit = argmax(predict_value)
-    return digit
+    prediction=model.predict(img_reshape)
+    return prediction
 
 if file is None:
     st.text("Please upload an image file")
 else:
     image = Image.open(file)
     st.image(image,use_column_width=True)
-    prediction = import_and_predict(image, model)
+    prediction = import_and_predict(image,model)
 
     class_names = ['Jute (Saluyot)', 'Maize (Mais)', 'Rice (Bigas)', 'Sugarcane (Tubo)', 'Wheat (Trigo)']
 
-    string="OUTPUT : " + class_names[prediction]
+    string="OUTPUT : " + class_names[np.argmax(prediction)]
     st.success(string)
