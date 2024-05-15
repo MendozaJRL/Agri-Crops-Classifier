2285798e29ffd8c4ca13e2d6419d506c3f7f3498
Renzo Mendoza <143251538+MendozaJRL@users.noreply.github.com>
2024-05-15 17:46:03 +0800

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -3,45 +3,36 @@
 import streamlit as st
 import tensorflow as tf
 
-from numpy import argmax
-from keras.preprocessing.image import load_img, img_to_array
-from keras.models import load_model
-from tensorflow.keras.models import model_from_json
-
 @st.cache_resource
-def Prediction(_filepath):   
-    img = load_img(_filepath, color_mode="grayscale", target_size=(width, length))
-  
-    img = img_to_array(img)
-    img = img.reshape(1, width, length, 1)
-    img = img.astype('float32')
-    img = img / 255.0
+def load_model():
+  model = tf.keras.models.load_model('filename1.h5')
+  return model
 
-    myModel = model
-    predict_value = myModel.predict(img)
-    digit = argmax(predict_value)
-    return class_names[digit]
-
-# Main
+model = load_model()
 st.write("""# Agricultural Crops Classifier""")
-file = st.file_uploader("Choose crop photo from computer", type = ["jpg", "png"])
+file = st.file_uploader("Choose crop photo from computer",type = ["jpg","png"])
 
 import cv2
 from PIL import Image,ImageOps
 import numpy as np
 
-model = load_model("filename1.h5")
-class_names = ['Jute (Saluyot)', 'Maize (Mais)', 'Rice (Bigas)', 'Sugarcane (Tubo)', 'Wheat (Trigo)']
-width = 100
-length = 100
+def import_and_predict(image_data,model):
+    size=(64,64)
+    image=ImageOps.fit(image_data, size)
+    img=np.asarray(image)
+  
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
+    return prediction
 
 if file is None:
     st.text("Please upload an image file")
 else:
     image = Image.open(file)
-    st.image(image, use_column_width=True)
-  
-    result = Prediction(image)
+    st.image(image,use_column_width=True)
+    prediction = import_and_predict(image,model)
 
-    string="OUTPUT : " + result
+    class_names = ['Jute (Saluyot)', 'Maize (Mais)', 'Rice (Bigas)', 'Sugarcane (Tubo)', 'Wheat (Trigo)']
+
+    string="OUTPUT : " + class_names[np.argmax(prediction)]
     st.success(string)
