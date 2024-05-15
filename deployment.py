e86fbf99df8ea1ca616250bd6213275913cf2c52
Renzo Mendoza <143251538+MendozaJRL@users.noreply.github.com>
2024-05-15 17:59:53 +0800

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -20,8 +20,7 @@ def import_and_predict(image_data,model):
     size=(100,100)
     image=ImageOps.fit(image_data, size)
     img=np.asarray(image)
-  
-    img_reshape=img[np.newaxis,...]
+    img_reshape = img.reshape(1, width, length, 1)
     prediction=model.predict(img_reshape)
     return prediction
 
