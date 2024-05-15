b643c0cc2f7d6106a7d4bd1de8234d8414006b76
Renzo Mendoza <143251538+MendozaJRL@users.noreply.github.com>
2024-05-15 14:54:33 +0800

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -13,44 +13,29 @@ st.write("""# Agricultural Crops Classifier""")
 file = st.file_uploader("Choose crop photo from computer",type = ["jpg","png"])
 
 import cv2
+from PIL import Image,ImageOps
 import numpy as np
-from numpy import argmax
-from PIL import Image
-from keras.preprocessing.image import load_img, img_to_array
-from keras.models import load_model
 
-Classification = ['Jute (Saluyot)', 'Maize (Mais)', 'Rice (Bigas)', 'Sugarcane (Tubo)', 'Wheat (Trigo)']
-width = 100
-length = 100
+def import_and_predict(image_data,model):
+    width = 100
+    height = 100
+    size = (width, height)
 
-# load and prepare the image
-def Prediction(filepath):
-    # Load and prepare the image
-    img = load_img(filepath, grayscale=True, target_size=(width, length))
+    image = ImageOps.fit(image_data,size, Image.ANTIALIAS)
+    img = np.asarray(image)
 
-    # Convert to array
-    img = img_to_array(img)
-
-    # Reshape into a single sample with 1 channel
-    img = img.reshape(1, width, length, 1)
-
-    # Prepare pixel data
-    img = img.astype('float32')
-    img = img / 255.0
-
-    # Obtains a value or digit which will be the index for classification
-    predict_value = myModel.predict(img)
-    digit = argmax(predict_value)
-
-    # Identify using the obtained digit
-    return Classification[digit]
+    img_reshape = img[np.newaxis, ...]
+    prediction = model.predict(img_reshape)
+    return prediction
 
 if file is None:
     st.text("Please upload an image file")
 else:
     image = Image.open(file)
     st.image(image,use_column_width=True)
-    prediction = Prediction(image)
+    prediction = import_and_predict(image,model)
 
-    string="OUTPUT : " + prediction
+    class_names = ['Jute (Saluyot)', 'Maize (Mais)', 'Rice (Bigas)', 'Sugarcane (Tubo)', 'Wheat (Trigo)']
+
+    string="OUTPUT : " + class_names[np.argmax(prediction)]
     st.success(string)
