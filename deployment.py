63f50edeb40360671eafa7a8029f558db8803395
Renzo Mendoza <143251538+MendozaJRL@users.noreply.github.com>
2024-05-15 16:42:19 +0800

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -9,18 +9,6 @@ from keras.models import load_model
 from tensorflow.keras.models import model_from_json
 
 @st.cache_resource
-st.write("""# Agricultural Crops Classifier""")
-file = st.file_uploader("Choose crop photo from computer",type = ["jpg","png"])
-
-import cv2
-from PIL import Image,ImageOps
-import numpy as np
-
-model = load_model("Model79.4.h5")
-class_names = ['Jute (Saluyot)', 'Maize (Mais)', 'Rice (Bigas)', 'Sugarcane (Tubo)', 'Wheat (Trigo)']
-width = 100
-length = 100
-
 def Prediction(filepath):   
     img = load_img(filepath, color_mode="grayscale", target_size=(width, length))
   
@@ -33,6 +21,19 @@ def Prediction(filepath):
     predict_value = myModel.predict(img)
     digit = argmax(predict_value)
     return class_names[digit]
+
+# Main
+st.write("""# Agricultural Crops Classifier""")
+file = st.file_uploader("Choose crop photo from computer",type = ["jpg","png"])
+
+import cv2
+from PIL import Image,ImageOps
+import numpy as np
+
+model = load_model("Model79.4.h5")
+class_names = ['Jute (Saluyot)', 'Maize (Mais)', 'Rice (Bigas)', 'Sugarcane (Tubo)', 'Wheat (Trigo)']
+width = 100
+length = 100
 
 if file is None:
     st.text("Please upload an image file")
