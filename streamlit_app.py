71cda4b64783854b3a848c9782ba5daa90249087
Renzo Mendoza <143251538+MendozaJRL@users.noreply.github.com>
2024-05-15 21:06:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,31 +22,31 @@ def prepare_image(image_data):
   return img_array
 
 def prediction(model, img_array):
-    predictions = model.predict(img_array)
-    predicted_class_digit = np.argmax(predictions[0])
-    predicted_class = class_names[predicted_class_digit]
-    confidence_scores = predictions[0] * 100
-    return predicted_class, confidence_scores
+  predictions = model.predict(img_array)
+  predicted_class_digit = np.argmax(predictions[0])
+  predicted_class = class_names[predicted_class_digit]
+  confidence_scores = predictions[0] * 100
+  return predicted_class, confidence_scores
 
 def main():
-    model = load_model()
-    class_names = ['Jute (Saluyot)', 'Maize (Mais)', 'Rice (Bigas)', 'Sugarcane (Tubo)', 'Wheat (Trigo)']
+  model = load_model()
+  class_names = ['Jute (Saluyot)', 'Maize (Mais)', 'Rice (Bigas)', 'Sugarcane (Tubo)', 'Wheat (Trigo)']
   
-    st.title("Agricultural Crops Classifier")
-    st.write("Please Upload a Crop Image")
-    file = st.file_uploader("Select Image ", type=["jpg", "jpeg", "png"])
+  st.title("Agricultural Crops Classifier")
+  st.write("Please Upload a Crop Image")
+  file = st.file_uploader("Select Image ", type=["jpg", "jpeg", "png"])
 
-    if file is not None:
-        image = Image.open(file)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
-        img_array = prepare_image(image)
+  if file is not None:
+    image = Image.open(file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+    img_array = prepare_image(image)
         
-        predicted_class, confidence_scores = prediction(model, img_array)
-        st.success(f"The predicted crop is: {predicted_class}")
+    predicted_class, confidence_scores = prediction(model, img_array)
+    st.success(f"The predicted crop is: {predicted_class}")
 
-        st.write("Confidence scores for each class:")
-        for i, class_name in enumerate(class_names):
-            st.write(f"{class_name}: {confidence_scores[i]:.2f}%")
+    st.write("Confidence scores for each class:")
+    for i, class_name in enumerate(class_names):
+      st.write(f"{class_name}: {confidence_scores[i]:.2f}%")
 
 if __name__ == "__main__":
   main()
