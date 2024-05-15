817638d9ea2b53303ca0a9b6b6a96e11f621635a
Renzo Mendoza <143251538+MendozaJRL@users.noreply.github.com>
2024-05-15 15:15:04 +0800

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -4,35 +4,51 @@ import streamlit as st
 import tensorflow as tf
 
 @st.cache_resource
-def load_model():
-  model = tf.keras.models.load_model('Model79.4.h5')
-  return model
+# Load the Model
+myModel = load_model('filename1.h5')
 
-model = load_model()
 st.write("""# Agricultural Crops Classifier""")
 file = st.file_uploader("Choose crop photo from computer",type = ["jpg","png"])
 
-import cv2
+from numpy import argmax
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
 from PIL import Image,ImageOps
-import numpy as np
 
-def import_and_predict(image_data,model):
-    size=(64,64)
-    image=ImageOps.fit(image_data, size)
-    img=np.asarray(image)
-  
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
-    return prediction
+# Array that corresponds to the labels
+Classification = ['Jute', 'Maize', 'Rice', 'Sugarcane', 'Wheat']
+
+# load and prepare the image
+def Prediction(filepath):
+    # Load and prepare the image
+    img = load_img(filepath, grayscale=True, target_size=(width, length))
+
+    # Convert to array
+    img = img_to_array(img)
+
+    # Reshape into a single sample with 1 channel
+    img = img.reshape(1, width, length, 1)
+
+    # Prepare pixel data
+    img = img.astype('float32')
+    img = img / 255.0
+
+    # Obtains a value or digit which will be the index for classification
+    predict_value = myModel.predict(img)
+    digit = argmax(predict_value)
+
+    # Identify using the obtained digit
+    return Classification[digit]
+
+import cv2
+from google.colab.patches import cv2_imshow
 
 if file is None:
     st.text("Please upload an image file")
 else:
-    image = Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction = import_and_predict(image,model)
+    test_image_path = Image.open(file)
+    st.image(test_image_path,use_column_width=True)
+    prediction = Prediction(test_image_path)
 
-    class_names = ['Jute (Saluyot)', 'Maize (Mais)', 'Rice (Bigas)', 'Sugarcane (Tubo)', 'Wheat (Trigo)']
-
-    string="OUTPUT : " + class_names[np.argmax(prediction)]
+    string="OUTPUT : " + prediction
     st.success(string)
